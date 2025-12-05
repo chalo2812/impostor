bf3a030b9b543cf7c1b79a2d0243e2308beaa929
Gonzalo Sola <gsola@systechsa.com>
2025-12-05 16:22:57 -0300

    fix(next): render src App layout in Next page and import src global styles

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,7 @@
 import '../styles/globals.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import '../src/index.css'
+import '../src/components/App/App.css'
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
