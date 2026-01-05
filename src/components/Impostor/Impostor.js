70ec644119922b1f811d63be66eaef048887c86b
Gonzalo Sola <gsola@systechsa.com>
2026-01-05 17:57:07 -0300

    Agregar la invocacion para que muestre la palabra aleatoria

--- src/components/Impostor/Impostor.js
+++ src/components/Impostor/Impostor.js
@@ -1,7 +1,22 @@
+import React, { useEffect, useState } from 'react';
+
 function Impostor() {
+  const [data, setData] = useState(null);
+  const [palabra, setPalabra] = useState("https://random-word-api.herokuapp.com/word?length=7&lang=es");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    fetch(palabra)
+      .then(response => response.json())
+      .then(data => setData(data))
+      .catch(error => setError(console.error('Error:', error)));
+  }, []);
+
   return (
     <div className="Impostor-root">
-        <div className="container">Parte Impostor</div>
+        <div className="container">{data}</div>
+        
     </div>
   );
 }
